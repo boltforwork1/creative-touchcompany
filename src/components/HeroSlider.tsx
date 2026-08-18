b1e3a6fe6474319a6c5edcf91f5c87a9e7068d8d
boltforwork14 <boltforwork14@gmail.com>
2026-08-18 14:56:35 +0300

    Updated HeroSlider.tsx

--- src/components/HeroSlider.tsx
+++ src/components/HeroSlider.tsx
@@ -92,7 +92,7 @@ export function HeroSlider() {
   useEffect(() => {
     const timer = setInterval(nextSlide, AUTOPLAY_MS)
     return () => clearInterval(timer)
-  }, [nextSlide])
+  }, [nextSlide, index])
 
   return (
     <section className="relative h-[600px] w-full overflow-hidden bg-[#1A1820] sm:min-h-[80vh] lg:h-[calc(100vh-7rem)] lg:min-h-[calc(100vh-7rem)]">
