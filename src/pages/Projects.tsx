afbee7ed5df15aa94ddbc44c35038b0ad25c4d6a
boltforwork15 <boltforwork15@gmail.com>
2026-08-20 20:07:57 +0300

    Updated Projects.tsx

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -56,7 +56,7 @@ const projects: Project[] = [
   { title: "Print Project 1", category: "Corporate Printing", icon: Printer, image: "/print1.jpg", companyName: "Lamsat Alqsor" },
   { title: "Print Project 2", category: "Corporate Printing", icon: Printer, image: "/print2.jpg", companyName: "Alomraa" },
   { title: "Print Project 3", category: "Corporate Printing", icon: Printer, image: "/print3.jpg", companyName: "NARC" },
-  { title: "Print Project 4", category: "Corporate Printing", icon: Printer, image: "/print4.jpg", companyName: "Corporate Client 4" },
+  { title: "Print Project 4", category: "Corporate Printing", icon: Printer, image: "/print4.jpg", companyName: "GTS" },
   { title: "Print Project 5", category: "Corporate Printing", icon: Printer, image: "/print5.jpg", companyName: "Corporate Client 5" },
   { title: "Print Project 6", category: "Corporate Printing", icon: Printer, image: "/print6.jpg", companyName: "Corporate Client 6" },
   { title: "Website Project 1", category: "Website Design", icon: Monitor, image: "/web1.jpg", companyName: "Lamsat Alqsor", liveLink: "https://lamsat-alqsoor.com" },
