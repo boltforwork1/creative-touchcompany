aea2c814dfb7f1cec3b2640a0189f078ed9e5155
boltforwork15 <boltforwork15@gmail.com>
2026-08-21 20:51:23 +0300

    Add ScrollToTop and vercel.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { ScrollToTop } from "@/components/ScrollToTop"
 import { Layout } from "@/components/layout/Layout"
 import { Home } from "@/pages/Home"
 import { About } from "@/pages/About"
@@ -9,6 +10,7 @@ import { Contact } from "@/pages/Contact"
 export function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route element={<Layout />}>
           <Route index element={<Home />} />
